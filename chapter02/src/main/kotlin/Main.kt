42298965e612b84b406af41193a8387bc37344cf
berkleylim <berkleylim16@gmail.com>
2024-03-04 17:45:25 +0900

    feat: add user info on chapter02 - 챕터 2 부분 사용자 입력 - 로직 설계

--- chapter02/src/main/kotlin/Main.kt
+++ chapter02/src/main/kotlin/Main.kt
@@ -1,9 +1,39 @@
-fun main(args: Array<String>) {
+import java.util.Scanner
+
+fun main() {
+    // 아래는 입력 받는 값 : Java에서는 Scanner sc = new Scanner(System.in) 과 비슷
+    // 결국 Java와 사용 비슷하다.
+    var sc : Scanner = Scanner(System.`in`)
+
+    println("AS 서비스 이용 전 인적사항을 먼저 확인합니다.")
+    print("이름을 입력하세요.")
+    var name : String = sc.nextLine()
+
+    print("나이를 입력하세요.")
+    var age : Int = sc.nextInt()
+    sc.nextLine()
+
+    print("휴대폰 번호 입력하세요. (- 제외)")
+    var phoneNumber : String = sc.nextLine()
 
 
-    println("Hello World!")
+    println("******************************************************************")
+    println(name + "(" + age + ") : " + phoneNumber + " 회원 환영합니다.")
+    println("******************************************************************")
+    println()
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+    // 비즈니스 로직 부분
+    while(true) {
+        println("계속 하실려면 1 클릭, 종료 시 아무키나 클릭하여 주세요.")
+        if (!readln().equals("1")) {
+            break
+        }
+        println()
+        println("******************************************************************")
+        println("메뉴를 선택해 주세요. 1. 신청, 2. 조회, 3. 변경, 4. 취소")
+    }
+
 }
+
+
