1b806a80d4533ebfa3f877d7551236cb10f93ace
BerkleyLim <berkleylim16@gmail.com>
2024-03-05 21:32:46 +0900

    feat: add sample json file - 샘플 json 파일을 불려온다. - 이후, loadJsonFile 에 해당 기능을 넣는다. - Kotlin 특성 상 call-by-reference를 지원하지 않아 c언어 처럼 사용해야 한다.

--- chapter02/src/main/kotlin/Main.kt
+++ chapter02/src/main/kotlin/Main.kt
@@ -30,69 +30,18 @@ fun main() {
     println()
 
     val path = "/data/dataSample.json"
-    var jsonArray : JSONArray;
+
+    // c언어 처럼 변수를 초기화 해줘야 함
 //    var as_service : AS[] = AS(0,null,null,null,null,null,0,null,null, null,null)
-    var as_service : Array<AS>?;
+    var as_service : Array<AS?> = arrayOfNulls<AS>(1)
 
-    //https://www.techiedelight.com/ko/write-json-to-a-file-in-kotlin/
-    try {
-        // 파일 읽어오기
-        fun getResourceAsText(path: String): String? =
-            object {}.javaClass.getResource(path)?.readText()
+    // json 파일 오픈
+    as_service = loadJsonFile(path)
 
-        jsonArray = JSONArray(getResourceAsText("/data/dataSample.json"))
-
-//        println(jsonArray.length())
-
-//        println(jsonArray.javaClass)
-//        arrayOf(as_service)
-
-        for (index:Int in 0..(jsonArray.length()-1)) {
-            var jsonObject = jsonArray.getJSONObject(index)
-
-            println(jsonObject)
-
-            var as_temp : AS = AS(
-                jsonObject.getInt("no"),
-                jsonObject.getString("name"),
-                jsonObject.getString("phoneNumber"),
-                jsonObject.getString("asServiceNumber"),
-                jsonObject.getString("modelNumber"),
-                jsonObject.getString("impsbDesc"),
-                jsonObject.getInt("impsbCuzCd"),
-                jsonObject.getString("impsbCuz"),
-                LocalDate.parse(jsonObject.getString("preferDeliDate")),
-                LocalDate.parse(jsonObject.getString("receiptDate")),
-                LocalDate.parse(jsonObject.getString("prcsCpltDate"))
-            );
-
-            println(as_temp)
-
-//            as_service.set(index, AS(
-//                jsonObject.getInt("no"),
-//                jsonObject.getString("name"),
-//                jsonObject.getString("phoneNumber"),
-//                jsonObject.getString("asServiceNumber"),
-//                jsonObject.getString("modelNumber"),
-//                jsonObject.getString("impsbDesc"),
-//                jsonObject.getInt("impsbCuzCd"),
-//                jsonObject.getString("impsbCuz"),
-//                LocalDate.parse(jsonObject.getString("preferDeliDate")),
-//                LocalDate.parse(jsonObject.getString("receiptDate")),
-//                LocalDate.parse(jsonObject.getString("prcsCpltDate"))
-//            ))
-
-        }
-    } catch (e: Exception) {
-        e.printStackTrace()
+    // java랑 다르게 변수는 무조건 초기화 해줘야함
+    for (index : Int in 0 until as_service.size) {
+        println(as_service[index])
     }
-
-
-//    println(as_service.toString())
-
-//    as_service.name = "홍길동"
-
-//    println(as_service.toString())
 
     // 비즈니스 로직 부분
     while(true) {
@@ -103,7 +52,7 @@ fun main() {
         println()
         println("******************************************************************")
         println("메뉴를 선택해 주세요. 1. 신청, 2. 조회, 3. 변경, 4. 취소")
-        var menuNumber : Int = sc.nextInt();
+        val menuNumber : Int = sc.nextInt();
         if (menuNumber == 1) {
             // 신청 메뉴
         } else if (menuNumber == 2) {
@@ -118,7 +67,74 @@ fun main() {
             println()
         }
     }
-
 }
 
+/**
+ * (1)
+ * loadJsonFile : 다음은 json 파일읇 불려오는 메서드 입니다.
+ * parameter : 경로
+ * return : as_service 값, json 파일을 AS 객체에 삽입합니다.
+ */
+fun loadJsonFile(
+    path: String
+):Array<AS?> {
+    // call-by-reference 지원 하지 않아 여기서 지원 후, as_service 만 리턴
+    var as_service : Array<AS?> = arrayOfNulls<AS>(1)
+
+    // java와 다르게 변수는 무조건 초기화 해줘야 됨:
+    var jsonArray = JSONArray();
+
+    //https://www.techiedelight.com/ko/write-json-to-a-file-in-kotlin/
+    try {
+        // 파일 읽어오기
+        fun getResourceAsText(path: String): String? =
+            object {}.javaClass.getResource(path)?.readText()
+
+        jsonArray = JSONArray(getResourceAsText(path))
+
+
+        as_service = arrayOfNulls<AS?>(jsonArray.length())
+//        println(jsonArray.length())
+//        println(jsonArray.javaClass)
+
+        for (index:Int in 0..(jsonArray.length()-1)) {
+            var jsonObject = jsonArray.getJSONObject(index)
+
+//            var as_temp : AS = AS(
+//                jsonObject.getInt("no"),
+//                jsonObject.getString("name"),
+//                jsonObject.getString("phoneNumber"),
+//                jsonObject.getString("asServiceNumber"),
+//                jsonObject.getString("modelNumber"),
+//                jsonObject.getString("impsbDesc"),
+//                jsonObject.getInt("impsbCuzCd"),
+//                jsonObject.getString("impsbCuz"),
+//                LocalDate.parse(jsonObject.getString("preferDeliDate")),
+//                LocalDate.parse(jsonObject.getString("receiptDate")),
+//                LocalDate.parse(jsonObject.getString("prcsCpltDate"))
+//            );
+
+            // 다음은 as_service 를 각 json을 가공하여 AS 객체에 저장한다.
+            as_service[index] = AS(
+                jsonObject.getInt("no"),
+                jsonObject.getString("name"),
+                jsonObject.getString("phoneNumber"),
+                jsonObject.getString("asServiceNumber"),
+                jsonObject.getString("modelNumber"),
+                jsonObject.getString("impsbDesc"),
+                jsonObject.getInt("impsbCuzCd"),
+                jsonObject.getString("impsbCuz"),
+                LocalDate.parse(jsonObject.getString("preferDeliDate")),
+                LocalDate.parse(jsonObject.getString("receiptDate")),
+                LocalDate.parse(jsonObject.getString("prcsCpltDate"))
+            )
+
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+
+    }
+
+    return as_service
+}
 
